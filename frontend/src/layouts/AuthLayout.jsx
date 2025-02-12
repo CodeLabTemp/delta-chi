f33cf779c8e6e700000591b787b579a423af8125
TuetrungNguyen <44383594+TuetrungNguyen@users.noreply.github.com>
2025-02-11 16:51:08 -0800

    Added and adjusted the sidebar, dashboard, and admin portal button components. while visually they look fine the functionality is yet to be complete. to see the new components by themselves go to the testpage

--- frontend/src/layouts/AuthLayout.jsx
+++ frontend/src/layouts/AuthLayout.jsx
@@ -7,13 +7,13 @@ import PrivateNavbar from "@/components/PrivateNavbar";
 import AdminDashboard from "@/components/admin/admindashboard";
 import { Sidebar } from "lucide-react";
 import MemberSideBar from "@/components/ui/sidebar";
+import { PortalButton } from "@/components/ui/portalbutton";
 
 const AuthLayout = () => {
   const { isCheckingAuth, checkAuth, isAuthenticated, user, logout } =
     useAuthStore();
 
   useEffect(() => {
-    // console.log("AuthLayout.jsx useEffect() fired");
     checkAuth();
   }, [checkAuth]);
 
@@ -22,17 +22,27 @@ const AuthLayout = () => {
   }
 
   return (
-    <div className="flex h-screen">
-      {/* {user && <Button onClick={logout}>Logout</Button>} */}
-      {/* {user && <PrivateNavbar logout={logout} />} */}
-      {/*user && <PrivateNavbar />*/}
-        {user && <MemberSideBar/>}
-      <div className="flex-1 flex flex-col transition-all duration-300">
-        {user && <AdminDashboard />}
-      </div>
+    <>
+      {user && (
+        <div className="flex h-screen relative">
+          <MemberSideBar />
+
+          <div className="flex-1 flex flex-col transition-all duration-300 p-6">
+            <div className="self-start mb-4"></div>
+
+            <div className="self-end mb-4">
+              <PortalButton className="px-6 py-2 text-white">
+                Admin Portal
+              </PortalButton>
+            </div>
+
+            <AdminDashboard />
+          </div>
+        </div>
+      )}
+
       <Outlet />
-      {/*<Toaster />*/}
-    </div>
+    </>
   );
 };
 
