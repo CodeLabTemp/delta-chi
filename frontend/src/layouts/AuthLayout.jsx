7c29ef50282f8cdc440d5a20cf09995c15fef9ce
CodeLabTemp <martingarcia9931@gmail.com>
2025-02-13 12:22:06 -0800

    Reset authlayout and public layout
    Moved public components to welcomePage

--- frontend/src/layouts/AuthLayout.jsx
+++ frontend/src/layouts/AuthLayout.jsx
@@ -4,9 +4,6 @@ import { useEffect } from "react";
 import { Toaster } from "@/components/ui/toaster";
 import { Button } from "../components/ui/button";
 import PrivateNavbar from "@/components/PrivateNavbar";
-import AdminDashboard from "@/components/admin/admindashboard";
-import { Sidebar } from "lucide-react";
-import MemberSideBar from "@/components/ui/sidebar";
 
 const AuthLayout = () => {
   const { isCheckingAuth, checkAuth, isAuthenticated, user, logout } =
@@ -22,16 +19,12 @@ const AuthLayout = () => {
   }
 
   return (
-    <div className="flex h-screen">
+    <div>
       {/* {user && <Button onClick={logout}>Logout</Button>} */}
       {/* {user && <PrivateNavbar logout={logout} />} */}
-      {/*user && <PrivateNavbar />*/}
-        {user && <MemberSideBar/>}
-      <div className="flex-1 flex flex-col transition-all duration-300">
-        {user && <AdminDashboard />}
-      </div>
+      {user && <PrivateNavbar />}
       <Outlet />
-      {/*<Toaster />*/}
+      <Toaster />
     </div>
   );
 };
--- frontend/src/layouts/PublicLayout.jsx
+++ frontend/src/layouts/PublicLayout.jsx
@@ -1,87 +1,18 @@
 import PublicNavbar from "@/components/PublicNavbar";
 import { Outlet } from "react-router-dom";
 import PublicFooter from "@/components/PublicFooter";
-import PublicHeader from "@/components/PublicHeader";
-import PublicAbout from "@/components/PublicAbout";
-import PublicContact from "@/components/PublicContact";
-import PublicEvents from "@/components/PublicEvents";
-import PublicGallery from "@/components/PublicGallery";
-import PublicExecutives from "@/components/PublicExecutives";
 
 
 const PublicLayout = ({ children }) => {
   console.log("PublicLayout.jsx fired");
 
-  let pictures = [
-    {photoName:"Photo1", photoSource:"/aboutpic.png"},
-    {photoName:"Photo2", photoSource:"/aboutpic.png"},
-    {photoName:"Photo3", photoSource:"/aboutpic.png"},
-    {photoName:"Photo4", photoSource:"/aboutpic.png"}
-  ]
-  const evBoard = [
-    {
-      eventImage: "/headerphoto.jpg",
-      eventName: "Main Event",
-      eventDate: "November 19, 2024",
-      eventDescription: "Lorem ipsum dolor sit amet consectetur. Congue enim eleifend quis scelerisque a. Mi sit lacinia id sed id ullamcorper.",
-      isMain: true, // Mark the main event
-    },
-    {
-      eventImage: "/headerphoto.jpg",
-      eventName: "Event 1",
-      eventDate: "November 19, 2024",
-      eventDescription: "Fermentum vulputate mus at iaculis sociis vitae...",
-    },
-    {
-      eventImage: "/headerphoto.jpg",
-      eventName: "Event 2",
-      eventDate: "December 1, 2024",
-      eventDescription: "Vestibulum ante ipsum primis in faucibus orci...",
-    },
-    {
-      eventImage: "/headerphoto.jpg",
-      eventName: "Event 3",
-      eventDate: "January 5, 2025",
-      eventDescription: "Curabitur ac leo nunc. Vestibulum et mauris vel...",
-    },
-  ];
-
-  const exBoard = [
-    {
-      memberRank: "President",
-      memberName: "Alex Nguyen",
-      memberImage: "/avatar-jessica.jpeg",
-    },
-    {
-      memberRank: "Vice President",
-      memberName: "Leo Lipshutz",
-      memberImage: "/avatar-jessica.jpeg",
-    },
-    {
-      memberRank: "Secretary",
-      memberName: "Nolan Dokidis",
-      memberImage: "/avatar-jessica.jpeg",
-    },
-    {
-      memberRank: "Treasurer",
-      memberName: "Jaylon Dias",
-      memberImage: "/avatar-jessica.jpeg",
-    },
-  ];
+  
 
   return (
     <div>
       <PublicNavbar />
-      <main>
-        <Outlet />    
-        <PublicHeader />
-        <PublicAbout />
-        <PublicEvents evBoard={evBoard}/>
-        <PublicExecutives executives ={exBoard}/>
-        <PublicGallery galleryPics ={pictures} />
-        <PublicContact />
-      </main>
-      <PublicFooter />
+        <Outlet /> 
+      <PublicFooter/>   
     </div>
   );
 };
